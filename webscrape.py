9215ba2940c47411c4df52dfdaa3037091ee8f37
scheung97 <scheung1997@gmail.com>
2021-04-01 17:30:45 -0400

    gets NYTimes page text using bs4

--- webscrape.py
+++ webscrape.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from bs4 import BeautifulSoup 
+import requests
+
+def main(): 
+    url = "https://www.nytimes.com/interactive/2021/us/massachusetts-covid-cases.html"
+    r = requests.get(url)
+    # print(r.text)
+    html = r.text
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.get_text())
+
+if __name__ == "__main__": 
+    main()    
