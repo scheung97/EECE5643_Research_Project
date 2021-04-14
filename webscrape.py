586128970d6c92343d86b9de77db5d736c17c9bc
scheung97 <scheung1997@gmail.com>
2021-04-14 15:21:03 -0400

    remove bs4 and use the method Chris found

--- webscrape.py
+++ webscrape.py
@@ -1,9 +1,16 @@
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
+import json
 import requests
 
 def main(): 
-    url = "https://covid.cdc.gov/covid-data-tracker/#datatracker-home"
+    url = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=statusBar_external_data"
     r = requests.get(url)
+    res = json.loads(r.text)
+    res = res['statusBar'][0]
+    tot_cases = res['us_total_cases']
+    tot_deaths = res['us_total_deaths']
+    tot_vaccines_administered = res['Administered_US']
+    """
     soup = BeautifulSoup(r.text, 'html.parser')
 
 
@@ -16,7 +23,9 @@ def main():
         cases = soup.find('div', id="status-cases-total")
         vac_admin = soup.find('div', id="status-total-vaccines")
         deaths = soup.find('div',id="status-deaths-total")
-    print(f"deaths: {str(deaths)}\n cases: {str(cases)}\n vaccines administered: {str(vac_admin)}\n")
+    """
+    print(f"deaths: {tot_deaths}\n cases: {tot_cases}\n vaccines administered: {tot_vaccines_administered}\n")
+
 
 
 if __name__ == "__main__": 
